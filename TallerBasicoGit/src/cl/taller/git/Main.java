34958a92c25957117c68ee9a2df9a1f59534cf4a
ricpavez <androidxperiaele@gmail.com>
2014-04-07 17:17:19 -0300

    Nuevo cambio web

--- TallerBasicoGit/src/cl/taller/git/Main.java
+++ TallerBasicoGit/src/cl/taller/git/Main.java
@@ -3,8 +3,8 @@ package cl.taller.git;
 public class Main {
 
 	public static void main(String[] args) {
-System.out.println("Mi segundo commit");
-System.out.println("Linea agregada de web");
+	System.out.println("Mi segundo commit");
+	System.out.println("Linea agregada de web, modificada en web");, 
 
 	}
 
