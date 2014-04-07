669fde728d1267331d92d9768c4d677179921353
Ricardo <Ricardo@10.3.5.31>
2014-04-07 17:20:58 -0300

    nueva modificacion web 2

--- TallerBasicoGit/src/cl/taller/git/Main.java
+++ TallerBasicoGit/src/cl/taller/git/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 System.out.println("Mi segundo commit");
-System.out.println("Linea agregada de web");
+System.out.println("Linea agregada de web, hola hola hola");
 
 	}
 
